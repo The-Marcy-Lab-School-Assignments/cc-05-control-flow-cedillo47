e4b501afa4b1972e4b189c478ae52f464eff9b94
leo <cedillo47@gmail.com>
2021-10-06 14:45:13 +0000

    compleated questions 1 & 2

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(value){
+  if (typeof(value) !== 'number'){
+      return null
+  } else if ( 0 <= value){
+      return value
+  } else {
+      return value * -1
+  }
+} 
+
+console.log(absoluteValue('hello'))
+
+
+function multiplesOfFourAndSix(){
+    const answer = [];
+    for (let i = 1; i <= 100; i++){
+        if (i % 4 === 0 && i % 6 === 0){
+             answer.push(i)   
+        }
+    }
+    return answer
 }
 
-
-
-
+console.log(multiplesOfFourAndSix())
